1cd65786e1df46ea5b13b1af2d6f2283981e2c32
anshin-cls <akihiko.suenaga@anshin-cls.net>
2020-07-31 10:13:50 +0900

    Update page

--- script.js
+++ script.js
@@ -1,122 +1,87 @@
 'use strict';
 
 let localStream = null;
-
 let peer = null;
-
 let existingCall = null;
 
-navigator.mediaDevices.getUserMedia({video: true, audio: true}).then(function (stream)
-{
+navigator.mediaDevices.getUserMedia({video: true, audio: true})
+.then(function (stream) {
     // Success
     $('#my-video').get(0).srcObject = stream;
-
     localStream = stream;
-}).
-catch(function (error)
-{
+}).catch(function (error) {
     // Error
     console.error('mediaDevice.getUserMedia() error:', error);
-
     return;
 });
 
-peer = new Peer(
-{
+peer = new Peer({
     key: 'b0f8a736-9fd3-44ea-a0f0-5cf7a74c1b9d',
     debug: 3
 });
-	
-peer.on('open', function()
-{
+
+peer.on('open', function(){
     $('#my-id').text(peer.id);
 });
-	
-peer.on('error', function(err)
-{
-        alert(err.message);
+
+peer.on('error', function(err){
+    alert(err.message);
 });
 
-peer.on('close', function()
-{
+peer.on('close', function(){
 });
 
-peer.on('disconnected', function()
-{
+peer.on('disconnected', function(){
 });
 
-$('#make-call').submit(function(e)
-{
+$('#make-call').submit(function(e){
     e.preventDefault();
-
     const call = peer.call($('#callto-id').val(), localStream);
-
     setupCallEventHandlers(call);
 });
 
-$('#end-call').click(function()
-{
+$('#end-call').click(function(){
     existingCall.close();
 });
 
-peer.on('call', function(call)
-{
+peer.on('call', function(call){
     call.answer(localStream);
-
     setupCallEventHandlers(call);
 });
 
-function setupCallEventHandlers(call)
-{
+function setupCallEventHandlers(call){
     if (existingCall) {
         existingCall.close();
     };
 
     existingCall = call;
 
-    call.on('stream', function(stream)
-    {
+    call.on('stream', function(stream){
         addVideo(call,stream);
-
         setupEndCallUI();
-
         $('#their-id').text(call.remoteId);
     });
-    // 省略
-}
 
-function setupCallEventHandlers(call)
-{
-    // 省略
-    call.on('close', function()
-    {
+    call.on('close', function(){
         removeVideo(call.remoteId);
-
         setupMakeCallUI();
     });
 }
 
-function addVideo(call,stream)
-{
+function addVideo(call,stream){
     $('#their-video').get(0).srcObject = stream;
 }
 
-function removeVideo(peerId)
-{
-    $('#' + peerId).remove();
+function removeVideo(peerId){
+    $('#their-video').get(0).srcObject = undefined;
 }
 
-function setupMakeCallUI()
-{
+function setupMakeCallUI(){
     $('#make-call').show();
-
     $('#end-call').hide();
 }
 
-function setupEndCallUI()
-{
+function setupEndCallUI() {
     $('#make-call').hide();
-
     $('#end-call').show();
 }
-
