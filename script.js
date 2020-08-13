b33723dcd172b9bb8e71972ab94e49ac3288a641
anshin-cls <akihiko.suenaga@anshin-cls.net>
2020-08-13 13:37:54 +0900

    Update page

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 let localStream = null;
 let peer = null;
 let existingCall = null;
-
+let conn = null;
 let datetime = new Date();
 
 navigator.mediaDevices.getUserMedia({video: true, audio: true})
@@ -20,8 +20,7 @@ navigator.mediaDevices.getUserMedia({video: true, audio: true})
 peer = new Peer(
     ('000' + datetime.getMilliseconds()).slice(-3),{
     key: 'b0f8a736-9fd3-44ea-a0f0-5cf7a74c1b9d',
-    debug: 1
-    // NONE=0,ERROR=1,WARN=2,ALL=3
+    debug: 1 // NONE=0,ERROR=1,WARN=2,ALL=3
 });
 
 peer.on('open', function(){
@@ -38,14 +37,11 @@ peer.on('close', function(){
 peer.on('disconnected', function(){
 });
 
-$('#make-call').submit(function(e){
-    e.preventDefault();
-    const call = peer.call($('#callto-id').val(), localStream);
-    setupCallEventHandlers(call);
-});
-
-$('#end-call').click(function(){
-    existingCall.close();
+peer.on('connection', function(connection){
+    // データ通信用に connectionオブジェクトを保存しておく
+    conn = connection;
+    // メッセージ受信イベントの設定
+    conn.on("data", onRecvMessage);
 });
 
 peer.on('call', function(call){
@@ -53,9 +49,52 @@ peer.on('call', function(call){
     setupCallEventHandlers(call);
 });
 
+$('#make-call').submit(function(e){
+    e.preventDefault();
+    const call = peer.call($('#callto-id').val(), localStream);
+    conn = peer.connect($('#callto-id').val());
+    // メッセージ受信イベントの設定
+    conn.on("data", onRecvMessage);
+    setupCallEventHandlers(call);
+});
+
+$('#end-call').click(function(){
+    existingCall.close();
+    conn.close();
+});
+
+// Sendボタンクリック時の動作
+$("#send").click(function(){
+    // 送信テキストの取得
+    let message = $("#message").val();
+    // メッセージが未入力の場合
+    if(!message){
+        return;
+    }
+
+    // 送信
+    conn.send(message);
+    // 自分の画面に表示
+    $("#messages").append($("<p>").html("私: " + message));
+    // 送信テキストボックスをクリア
+    $("#message").val("");
+});
+
+$("#delete").click(function(){
+    // メッセージをクリア
+    $("#messages").empty();
+});
+
+// メッセージ受信イベントの設定
+function onRecvMessage(data) {
+    // 画面に受信したメッセージを表示
+    $("#messages").append($("<p>").text(conn.remoteId + ": " + data).css("font-weight", "bold"));
+}
+
 function setupCallEventHandlers(call){
     if (existingCall) {
         existingCall.close();
+        conn.close();
     };
 
     existingCall = call;
@@ -69,6 +108,7 @@ function setupCallEventHandlers(call){
     call.on('close', function(){
         removeVideo(call.remoteId);
         setupMakeCallUI();
+        $("#messages").empty();
     });
 }
 
@@ -83,9 +123,11 @@ function removeVideo(peerId){
 function setupMakeCallUI(){
     $('#make-call').show();
     $('#end-call').hide();
+    $('#send-message').hide();
 }
 
 function setupEndCallUI() {
     $('#make-call').hide();
     $('#end-call').show();
+    $('#send-message').show();
 }
